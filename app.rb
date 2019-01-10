f5efac60096c1a3b81ff4843b8f0ad5c851266e7
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-10 10:50:54 -0500

    Interim checkin

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end
 
+   post '/team' do
+    @players = params["string"]
+    erb :team
+   end
 end
