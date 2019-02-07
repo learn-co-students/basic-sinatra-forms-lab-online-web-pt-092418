d8eaa3ae29cac1b8dd29cc2e95675ecee26711b6
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-02-06 23:41:16 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,14 @@ get '/newteam' do
   erb :newteam
 end
 
+
 post '/team' do
+  @name = params[:name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @pf = params[:pf]
+  @sg = params[:sg]
+  @c = params[:c]
   erb :team
 end
 
